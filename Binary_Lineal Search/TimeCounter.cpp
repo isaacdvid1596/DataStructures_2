d1ab93a958efee75632e4ad2999ff6174db0f9c7
isaacdvid1596 <44175229+isaacdvid1596@users.noreply.github.com>
2019-08-06 13:39:06 -0600

    Add files via upload

---
+++ Binary_Lineal Search/TimeCounter.cpp
@@ -0,0 +1,32 @@
+#include "TimeCounter.h"
+
+
+
+TimeCounter::TimeCounter()
+{
+}
+
+
+TimeCounter::~TimeCounter()
+{
+}
+
+void TimeCounter::start()
+{
+
+}
+
+void TimeCounter::end()
+{
+
+}
+
+double TimeCounter::length()
+{
+
+}
+
+void TimeCounter::reset()
+{
+
+}
